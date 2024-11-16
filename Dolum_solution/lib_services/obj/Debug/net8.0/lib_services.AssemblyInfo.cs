2d23c474de0e9ea97cfbe474b32b0c9790a7b2e0
SayanEl12 <santiago4herrera12@gmail.com>
2024-11-16 16:50:51 -0500

    Add nuggets packets

--- Dolum_solution/lib_services/obj/Debug/net8.0/lib_services.AssemblyInfo.cs
+++ Dolum_solution/lib_services/obj/Debug/net8.0/lib_services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5e5214f2d4c06bca619147ae17e42c641fd760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccbb3247b35ca37f6c78cb659f04c02f570653f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
