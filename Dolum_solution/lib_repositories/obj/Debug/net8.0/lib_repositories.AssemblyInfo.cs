f979e1fdd978f78aaa6d5aabe866cbf40c61ef81
SayanEl12 <santiago4herrera12@gmail.com>
2024-11-16 23:34:29 -0500

    asdf

--- Dolum_solution/lib_repositories/obj/Debug/net8.0/lib_repositories.AssemblyInfo.cs
+++ Dolum_solution/lib_repositories/obj/Debug/net8.0/lib_repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2dccbc5639325b1337f7a856f6dcd93aa5332c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe1e3cb63d18e4847e998cede95321f23c15ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
