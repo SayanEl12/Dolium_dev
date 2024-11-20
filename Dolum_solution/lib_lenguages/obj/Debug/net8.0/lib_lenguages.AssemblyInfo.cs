d319bcb0bcedc26ec77153064197d70816bda6f5
SayanEl12 <santiago4herrera12@gmail.com>
2024-11-20 11:09:46 -0500

    Add .resx file for errors messages

--- Dolum_solution/lib_lenguages/obj/Debug/net8.0/lib_lenguages.AssemblyInfo.cs
+++ Dolum_solution/lib_lenguages/obj/Debug/net8.0/lib_lenguages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_lenguages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2133e8865a4f7f731f29e5c55bdcad5f5491e059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c71b9850ca1fd341264cc2ca737479a96e9d649")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_lenguages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_lenguages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
