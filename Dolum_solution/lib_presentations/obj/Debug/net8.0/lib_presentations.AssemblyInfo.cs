e3e1fcfac336cfed03e7691d0516eb80ae13dc57
SayanEl12 <santiago4herrera12@gmail.com>
2024-11-20 10:37:54 -0500

    a

--- Dolum_solution/lib_presentations/obj/Debug/net8.0/lib_presentations.AssemblyInfo.cs
+++ Dolum_solution/lib_presentations/obj/Debug/net8.0/lib_presentations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_presentations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1ee9c0d2f77f730c2755765244b283878dae17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2133e8865a4f7f731f29e5c55bdcad5f5491e059")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_presentations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_presentations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Dolum_solution/lib_repositories/obj/Debug/net8.0/lib_repositories.AssemblyInfo.cs
+++ Dolum_solution/lib_repositories/obj/Debug/net8.0/lib_repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c324f3fd7ba358731831e04e3f79ffa2cd904b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2133e8865a4f7f731f29e5c55bdcad5f5491e059")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
